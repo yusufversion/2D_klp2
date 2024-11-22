0f7191690ea2ecf14705d1b63888ba827a5ae11a
yusufversion <yusufef698@gmail.com>
2024-11-22 14:27:30 +0700

    Perbaikan footer

--- app/views/nav/footer.php
+++ app/views/nav/footer.php
@@ -1,7 +1,7 @@
 
 <footer class="mt-auto bg-dark text-white py-3" style="margin-top: 50px;">
     <div class="container text-center">
-        <p class="mb-2">&copy; 2024 Sistem Peminjaman Buku. All rights reserved.</p>
+        <p class="mb-2">&copy; 2024 Sistem Peminjaman Buku Digital. All rights reserved.</p>
         <ul class="list-inline">
             <li class="list-inline-item">
                 <a href="#" class="text-white text-decoration-none">Privacy Policy</a>
